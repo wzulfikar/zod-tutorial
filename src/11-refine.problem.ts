db994c3feccbf76c56fb43e8a7a04c90c79e76bf
Wildan Zulfikar <hey@wzulfikar.com>
2022-09-18 19:13:32 +0300

    Exercise 11: my solution

--- src/11-refine.problem.ts
+++ src/11-refine.problem.ts
@@ -3,10 +3,15 @@
 import { expect, it } from "vitest";
 import { z } from "zod";
 
-const Form = z.object({
-  password: z.string(),
-  confirmPassword: z.string(),
-});
+const Form = z
+  .object({
+    password: z.string(),
+    confirmPassword: z.string(),
+  })
+  .refine(
+    (data) => data.password == data.confirmPassword,
+    "Passwords don't match"
+  );
 //^ 🕵️‍♂️
 
 export const validateFormInput = (values: unknown) => {
@@ -22,6 +27,6 @@ it("Should error if the passwords are not the same", () => {
     validateFormInput({
       password: "password",
       confirmPassword: "password1",
-    }),
+    })
   ).toThrowError("Passwords don't match");
 });
