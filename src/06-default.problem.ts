0a4380e001a85fe7e1c85fe6951057603547f6b0
Wildan Zulfikar <hey@wzulfikar.com>
2022-09-18 18:29:12 +0300

    Exercise 6: add z.input vs z.infer example

--- src/06-default.problem.ts
+++ src/06-default.problem.ts
@@ -5,9 +5,13 @@ import { z } from "zod";
 
 const Form = z.object({
   repoName: z.string(),
-  keywords: z.array(z.string()).optional().default([]),
+  keywords: z.array(z.string()).default([]),
   //                           ^ 🕵️‍♂️
 });
+
+export type FormInput = z.input<typeof Form>; // FormInput excludes `.default([])` from its inference
+
+export type FormOutput = z.infer<typeof Form>; // FormOutput includes `.default([])` in its inference
 
 export const validateFormInput = (values: unknown) => {
   const parsedData = Form.parse(values);
